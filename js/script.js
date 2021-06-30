2ef2a51977fd5ab8325e34363eb2e02082be0978
sookiemonster <dsooknanan20@stuy.edu>
2021-06-29 20:41:32 -0400

    Remove reflections, extended silhouette, & update navigation button
    Removed reflections as they would unsync with the silhouette, which now 
    scales with viewport width. Hamburger icon for the navigation button 
    turns into an X to close the menu.

--- js/script.js
+++ js/script.js
@@ -10,20 +10,29 @@ $(document).ready(function() {
 
     $("#silhouette").css({
       "top" : "80vh",
-      "width" : "100vw",
-      "max-width" : "100vw",
       "transition" : "top .2s",
     });
 
     $("#moon").css({
       "transition" : "0.25s",
       "clip-path" : "circle(100%)",
-      // "animation-play-state" : "paused",
     });
 
     $("#nav-circle").addClass("active-button").removeClass("inactive-button");
 
     $(".button-bar").addClass("active-bar").removeClass("inactive-bar");
+    $("#top-bar").css({
+      "top" : "27%",
+      "left" : "61%",
+      "transform-origin" : "top left",
+      "transform" : "translate(-50%, -50%) rotate(45deg)"
+    });
+    $("#bottom-bar").css({
+      "bottom" : "27%",
+      "left" : "61%",
+      "transform-origin" : "bottom left",
+      "transform" : "translate(-50%, -50%) rotate(-45deg)"
+    });
 
     $(".wave-container").fadeOut().css({
       "top" : "100vh",
@@ -42,28 +51,41 @@ $(document).ready(function() {
 
     $("#moon").css({
       "clip-path" : "circle(min(250px, 30vw))",
-      // "animation-play-state" : "running",
+      "animation-play-state" : "running",
     });
 
     $("#nav-circle").removeClass("active-button").addClass("inactive-button");
 
     $(".button-bar").removeClass("active-bar").addClass("inactive-bar");
+    $("#top-bar").css({
+      "top" : "35%",
+      "left" : "50%",
+      "transform" : "translate(-50%, -50%) rotate(0deg)"
+    });
+    $("#bottom-bar").css({
+      "bottom" : "35%",
+      "left" : "50%",
+      "transform" : "translate(-50%, -50%) rotate(0deg)"
+    });
 
     $(".wave-container").css("top", "0").fadeIn();
 
+    $("#silhouette").css({
+      "top" : "51vh",
+      "transition" : ".25s",
+    });
 
     setTimeout(function() {
-      $(document.body).css("background-color", "var(--primary)");
+      $(document.body).css("background-color", "var(--bg)");
       $("#title").css("color", "var(--tertiary)");
       $("#nav-page").fadeOut(200);
-
-      $("#silhouette").css({
-        "top" : "51vh",
-        "width" : "700px",
-        "max-width" : "80vw",
-        "transition" : ".25s",
-      });
     }, 100);
+
+    setTimeout(function() {
+      $("#silhouette").css({
+        "transition" : "0s",
+      });
+    }, 250);
 
     menu_toggled = false;
   }
