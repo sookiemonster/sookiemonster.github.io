125d6caa109a269cc33633e7170a69f8303aea5c
sookiemonster <dsooknanan20@stuy.edu>
2021-08-21 23:40:26 -0400

    Remove unnecessary comments

--- js/projects.js
+++ js/projects.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-    // $(".card").each(function(index) {
-    //     $(this).css("animation", 
-    //         "slide-from-left 1s " + (0.15 * index) + "s ease forwards"
-    //     );
-    // });
-    // $(".project-link ").each(function(index) {
-    //     $(this).css("animation", 
-    //         "slide-from-right 1s " + (0.15 * index) + "s ease forwards"
-    //     );
-    // });
-
     $(".project-item").each(function(index) {
         $(this).children(".card").css("animation", 
             "slide-from-left 0.75s " + (0.15 * index) + "s ease forwards"
