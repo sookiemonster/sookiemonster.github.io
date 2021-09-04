0e4bca6870c130feaf6fc33ae3984ed05075212d
sookiemonster <dsooknanan20@stuy.edu>
2021-09-03 20:28:52 -0400

    Add about-page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-// $(document).ready(function() {
-//     $("#home-image").height(document.documentElement.clientHeight);
-// });
+$(document).ready(function() {
+    var nav = $('nav');
+    var scrollY;
+    $(window).scroll(function() {
+        scrollY = $(document).scrollTop();
+        if (!nav.hasClass('scrolled') && scrollY > 0) {
+            nav.addClass('scrolled');
+        } else if (scrollY <= 0) {
+            nav.removeClass('scrolled');
+        }
+    }).scroll();
+});
